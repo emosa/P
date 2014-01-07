05d7fa0709ce30908d09fd5bad1ce67879e416b0
Elimarie <iglitzi@fullsail.edu>
2014-01-06 22:02:12 -0500

    Practice
    declaring mutiple variables

--- Wk1/goal1_basics/main.js
+++ Wk1/goal1_basics/main.js
@@ -13,3 +13,6 @@ var name, course = "PWA1", month;
 console.log("name: ", name + " / course: ", course + " , month: ", month);
 
 
+var a = b = c = d = 10;
+
+console.log("a:", a + " / b: ", b + " / c: ", c + " / d: ", d);
