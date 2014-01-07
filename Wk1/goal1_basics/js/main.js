142f765bbd46e55fe1c4e409f3b057ae3612085d
Elimarie <iglitzi@fullsail.edu>
2014-01-06 21:51:29 -0500

    Test
    test week folders

---
+++ Wk1/goal1_basics/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by iglitzi on 1/6/14.
+ */
---
+++ Wk1/goal1_basics/main.js
@@ -0,0 +1,8 @@
+
+
+var name = "james";
+var course = "PWA1";
+var month = 3;
+
+
+console.log("name: ", name + " / course: ", course + " , month: ", month);
