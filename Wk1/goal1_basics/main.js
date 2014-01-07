1be41509fed0724dc3db70202a875c71bf806c27
Elimarie <iglitzi@fullsail.edu>
2014-01-06 21:55:01 -0500

    practice video
    Following practice video

--- Wk1/goal1_basics/main.js
+++ Wk1/goal1_basics/main.js
@@ -1,8 +1,9 @@
 
 
-var name = "james";
-var course = "PWA1";
-var month = 3;
+//var name = "james";
+//var course = "PWA1";
+//var month = 3;
 
+var name, course, month;
 
 console.log("name: ", name + " / course: ", course + " , month: ", month);
