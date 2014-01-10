79aee191574a24f76748b13a85bc4232790b6d6f
Elimarie <iglitzi@fullsail.edu>
2014-01-10 18:52:24 -0500

    Goal1: Assignment: The Duel Part 1
    Setting up requirements

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,41 @@
+/* Elimarie Morales Santiago
+   Jan. 08, 2014
+   Goal1: Assignment: The Duel - Part1
+ */
+
+// self-executing function
+
+(function(){})();
+
+var playerOneName = “Spiderman;
+var playerOneHealth = 100;
+var playerOneDamge = 50;
+
+var playerTwoName = "Batman";
+var playerTwoHealth = 100;
+var playerTwoDamage = 50;
+
+
+fight() function
+for (var i = 0; i < 10; i++){ }
+
+
+
+winnerCheck() function
+
+    winner
+loser
+both die
+no winner yet
+
+break command
+
+Batman: 100 **START** Spiderman 100
+ Batman: 88 **ROUND 1 OVER** Spiderman 92
+ Batman: 79 **ROUND 2 OVER** Spiderman 84
+ Batman: 5 **ROUND 9 OVER** Spiderman 11
+ GAME OVER Batman Wins!!!
+
+    Math.floor(Math.random() * (max - min) + min);
+
+
--- Wk1/goal1_assign_duel1/main.js
+++
@@ -1,5 +0,0 @@
-/* Elimarie Morales Santiago
-   Jan. 08, 2014
-   Goal1: Assignment: The Duel - Part1
- */
-
