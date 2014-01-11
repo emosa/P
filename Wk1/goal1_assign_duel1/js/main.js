46f746b5d4a8dbe89dc4d25a234871cb9ca9bf22
Elimarie <iglitzi@fullsail.edu>
2014-01-10 20:46:39 -0500

    Goal1: Assignment: The Duel - Part I
    added function, else statement

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -6,7 +6,7 @@
 // self-executing function
 
 (function(){
-
+        console.log("FIGHT!!!");
 // Players Names
     var playerOneName = "Spiderman";
     var playerTwoName = "Batman";
@@ -20,35 +20,62 @@
     var playerTwoDamage = 20;
 
 // Initiate Round
-    var round = 1;
+    var round = 0;
 
-    function fight(){};
-    for (var i = 0; i < 10; i++){ }
+    function fight(){
 
-    function winnerCheck(){};
+     alert(playerOneName + ":" + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
+
+        for (var i = 0; i < 10; i++){
+
+// Random formula
+            var minDamage1 = playerOneDamge * .5;
+            var minDamage2 = playerTwoDamage * .5;
+            var f1 = Math.floor(Math.random() * (playerOneDamge - minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random() * (playerTwoDamage - minDamage2) + minDamage2);
+
+//inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+      console.log(playerOneName + ":" + playerOneHealth + ":" + playerTwoName + ":" + playerTwoHealth);
+
+        var result= winnerCheck();
+        console.log(result);
+      if (result === " No Winner "){
+          round ++;
+          alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER* " + playerTwoName + ":" + playerTwoHealth);
+      }else {
+          alert(result);
+          break;
+      };
+        };
+
+    };
+
+    function winnerCheck(){
+        console.log("in winnerCheck FN");
+        var result= " No Winner ";
+
+        if (playerOneHealth < 1 && playerTwoHealth < 1){
+          result = " You Both Die ";
+
+        }else if (playerOneHealth <1){
+          result = playerTwoName + " WINS!!! ";
+
+        }else if (playerTwoHealth <1){
+          result = playerOneName + " WINS!!! ";
+
+        };
+
+        return result;
+
+    };
+
+    /*** The program starts below ***/
+    fight();
 
 })();
 
-
-
-
-    winner
-loser
-both die
-no winner yet
-
-
-
-break command
-
-Math.floor(Math.random() * (max - min) + min);
-/*
-
-Batman: 100 **START** Spiderman 100
- Batman: 88 **ROUND 1 OVER** Spiderman 92
- Batman: 79 **ROUND 2 OVER** Spiderman 84
- Batman: 5 **ROUND 9 OVER** Spiderman 11
- GAME OVER Batman Wins!!!
-/*
 
 
